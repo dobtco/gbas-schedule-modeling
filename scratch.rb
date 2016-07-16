e5415d60bfec61c893adb8f849528b02c7c9570e
Adam Becker <ad@mbecker.cc>
2016-07-16 16:12:27 -0700

    reviewer workload stats, testing

--- scratch.rb
+++ scratch.rb
@@ -1,15 +1,19 @@
 require 'pry'
 require 'active_support/all'
+require 'descriptive-statistics'
 
 args = Hash[ ARGV.join(' ').scan(/--?([^=\s]+)(?:=(\S+))?/) ]
 
-NUM_APPLICANTS = args['applicants'] ? args['applicants'].to_i : 70
-NUM_TIMESLOTS = args['timeslots'] ? args['timeslots'].to_i : 100
-NUM_REVIEWERS = args['reviewers'] ? args['reviewers'].to_i : 30
+NUM_APPLICANTS = args['applicants'] ? args['applicants'].to_i : 50
+NUM_TIMESLOTS = args['timeslots'] ? args['timeslots'].to_i : 60
+NUM_REVIEWERS = args['reviewers'] ? args['reviewers'].to_i : 26
 REVIEWERS_PER_INTERVIEW = 2
 APPLICANT_CHOOSE_COUNT = 2
-REVIEWER_CHOOSE_COUNT = 5
-RESPONSE_RATE_FOR_AVAILABILITY_REQUEST = 90
+REVIEWER_CHOOSE_COUNT = 4
+RESPONSE_RATES_FOR_AVAILABILITY_REQUEST = {
+  'Applicant' => 90,
+  'Reviewer' => 97
+}
 
 # Users won't always pick the # of slots that they're asked to pick
 def randomized_choose_count(ask)
@@ -142,9 +146,9 @@ class Simulation
 
     ## Pick Availability
 
-    all_users.
-      reject { |_| rand(1..100) > RESPONSE_RATE_FOR_AVAILABILITY_REQUEST }.
-      each(&:pick_availability!)
+    all_users.reject do |u|
+      rand(1..100) > RESPONSE_RATES_FOR_AVAILABILITY_REQUEST[u.class.name]
+    end.each(&:pick_availability!)
 
     ## This is the "algorithm", lol?
 
@@ -162,10 +166,23 @@ class Simulation
 
     ### Now, book the reviewers
 
+    timeslots_in_use.each do |timeslot|
+      timeslot.reviewers = @reviewers.shuffle.select do |reviewer|
+        reviewer.available_for_timeslot?(timeslot.sequence)
+      end.first(2)
+    end
+
     ## Results
+
+    workload_stats = DescriptiveStatistics::Stats.new(@reviewers.map { |r| reviewer_workload(r) })
+
     {
       percent_responsive_applicants_booked: booked_applicants.length / responsive_applicants.length.to_f,
-      percent_slots_with_enough_reviewers: full_timeslots.length / timeslots_in_use.length.to_f
+      percent_slots_with_enough_reviewers: full_timeslots.length / timeslots_in_use.length.to_f,
+      average_reviewer_workload: workload_stats.mean,
+      reviewers_with_workflow_gt_1_sd: @reviewers.count { |r| reviewer_workload(r) > (workload_stats.mean + workload_stats.standard_deviation) } / @reviewers.length.to_f,
+      reviewers_with_workflow_gt_2_sd: @reviewers.count { |r| reviewer_workload(r) > (workload_stats.mean + (workload_stats.standard_deviation * 2)) } / @reviewers.length.to_f,
+      reviewers_with_workflow_gt_3_sd: @reviewers.count { |r| reviewer_workload(r) > (workload_stats.mean + (workload_stats.standard_deviation * 3)) } / @reviewers.length.to_f
     }
   end
 
@@ -188,20 +205,9 @@ class Simulation
     end
   end
 
-  # def rebook_applicant!(applicant, depth = 0)
-  #   current_sequence = timeslots.detect { |ts| ts.applicant == applicant }.try(:sequence)
-
-  #   available_timeslot = (applicant.availability - [current_sequence]).detect do |sequence|
-  #     !timeslot_by_sequence(sequence).in_use?
-  #   end
-
-  #   if available_timeslot
-  #     available_timeslot.applicant = applicant
-  #     true
-  #   else
-  #     false
-  #   end
-  # end
+  def reviewer_workload(reviewer)
+    timeslots.count { |timeslot| timeslot.reviewers.include?(reviewer) }
+  end
 
   def responsive_applicants
     @applicants.select(&:responded_to_availability_request?)
@@ -254,5 +260,13 @@ def fmt_percent(num)
   sprintf('%.2f', num * 100) + '%'
 end
 
+def fmt_number(num)
+  sprintf('%.2f', num)
+end
+
 puts "% of responsive applicants booked: #{fmt_percent(results.sum { |res| res[:percent_responsive_applicants_booked] / results.length.to_f})}"
 puts "% of slots with enough reviewers: #{fmt_percent(results.sum { |res| res[:percent_slots_with_enough_reviewers] / results.length.to_f})}"
+puts "Average reviewer workload: #{fmt_number(results.sum { |res| res[:average_reviewer_workload] / results.length.to_f})} interviews/reviewer"
+puts "% of reviewers with workflow > 1 standard deviation: #{fmt_percent(results.sum { |res| res[:reviewers_with_workflow_gt_1_sd] / results.length.to_f})}"
+puts "% of reviewers with workflow > 2 standard deviation: #{fmt_percent(results.sum { |res| res[:reviewers_with_workflow_gt_2_sd] / results.length.to_f})}"
+puts "% of reviewers with workflow > 3 standard deviation: #{fmt_percent(results.sum { |res| res[:reviewers_with_workflow_gt_3_sd] / results.length.to_f})}"
